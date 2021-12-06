dfcd6fce3489a8d3627b404706beab2e3a7794ec
Prabin Dangol <neprabin@gmail.com>
2021-12-06 11:00:08 +0545

    update

--- classes/library-class.php
+++ classes/library-class.php
@@ -1,79 +1,76 @@
-<?php 
+<?php
+
 defined('ABSPATH') or die('No Script Found!');
 
-if(!class_exists('MAJC_Library')) {
-	
-	class MAJC_Library {
+if (!class_exists('MAJC_Library')) {
 
-	    static function sanitize_array($array = array(), $sanitize_rule = array()) 
-	    {
-	      if (!is_array($array) || count($array) == 0) 
-	      {
-	          return array();
-	      }
+    class MAJC_Library {
 
-	      foreach ($array as $k => $v) {
-	          if (!is_array($v)) {
-	              $default_sanitize_rule = (is_numeric($k)) ? 'html' : 'text';
-	              $sanitize_type = isset($sanitize_rule[$k]) ? $sanitize_rule[$k] : $default_sanitize_rule;
-	              $array[$k] = self:: sanitize_value($v, $sanitize_type);
-	          }
+        static function sanitize_array($array = array(), $sanitize_rule = array()) {
+            if (!is_array($array) || count($array) == 0) {
+                return array();
+            }
 
-	          if (is_array($v)) {
-	              $array[$k] = self:: sanitize_array($v, $sanitize_rule);
-	          }
-	      }
+            foreach ($array as $k => $v) {
+                if (!is_array($v)) {
+                    $default_sanitize_rule = (is_numeric($k)) ? 'html' : 'text';
+                    $sanitize_type = isset($sanitize_rule[$k]) ? $sanitize_rule[$k] : $default_sanitize_rule;
+                    $array[$k] = self:: sanitize_value($v, $sanitize_type);
+                }
 
-	      return $array;
-	    }
+                if (is_array($v)) {
+                    $array[$k] = self:: sanitize_array($v, $sanitize_rule);
+                }
+            }
 
-	    static function sanitize_value($value = '', $sanitize_type = 'html') 
-	    {
-	        switch ($sanitize_type) 
-	        {
-	          case 'text':
-	              $allowed_html = wp_kses_allowed_html('post');
-	              return wp_kses($value, $allowed_html);
-	              break;
-	          default:
-	              return sanitize_text_field($value);
-	              break;
-	        }
-	    }
+            return $array;
+        }
 
-	    public function pr($array) {
-	        echo "<pre>";
-	        print_r($array);
-	        echo "</pre>";
-	    }
+        static function sanitize_value($value = '', $sanitize_type = 'html') {
+            switch ($sanitize_type) {
+                case 'text':
+                    $allowed_html = wp_kses_allowed_html('post');
+                    return wp_kses($value, $allowed_html);
+                    break;
+                default:
+                    return sanitize_text_field($value);
+                    break;
+            }
+        }
 
-	    public function majc_sanitize_url($url) {
-	    	$sanitized_url = strip_tags( stripslashes( filter_var($url, FILTER_VALIDATE_URL) ) );
-	    	return $sanitized_url;
-	    }
+        public function pr($array) {
+            echo "<pre>";
+            print_r($array);
+            echo "</pre>";
+        }
 
-	    public function majc_animations() {
-	    	$animations =   
-				[
-					'show_animation'	=>  array(
-												'Fading Entrances'		=> array('fadeIn','fadeInLeft','fadeInRight'),
-												'Slide Entrance'		=> array('slideInLeft','slideInRight')
-												),
-					'hide_animation'	=>  array(
-												'Fading Exits'			=> array('fadeOut','fadeOutLeft','fadeOutRight'),
-												'Slide Exits'			=> array('slideOutLeft','slideOutRight')
-												),
-					'hover_animation'	=>  array(
-												'2D Transitions'		=> array(
-														'Grow'					=> 'hvr-grow',
-														'Shrink'				=> 'hvr-shrink',
-														'Pulse'					=> 'hvr-pulse'
-														)
-											),
-				];
-			return $animations;
-	    }
-	}
+        public function majc_sanitize_url($url) {
+            $sanitized_url = strip_tags(stripslashes(filter_var($url, FILTER_VALIDATE_URL)));
+            return $sanitized_url;
+        }
 
-	new MAJC_Library();
+        public function majc_animations() {
+            $animations = [
+                'show_animation' => array(
+                    'Fading Entrances' => array('fadeIn', 'fadeInLeft', 'fadeInRight'),
+                    'Slide Entrance' => array('slideInLeft', 'slideInRight')
+                ),
+                'hide_animation' => array(
+                    'Fading Exits' => array('fadeOut', 'fadeOutLeft', 'fadeOutRight'),
+                    'Slide Exits' => array('slideOutLeft', 'slideOutRight')
+                ),
+                'hover_animation' => array(
+                    '2D Transitions' => array(
+                        'Grow' => 'hvr-grow',
+                        'Shrink' => 'hvr-shrink',
+                        'Pulse' => 'hvr-pulse'
+                    )
+                ),
+            ];
+            return $animations;
+        }
+
+    }
+
+    new MAJC_Library();
 }
--- mini-ajax-cart.php
+++ mini-ajax-cart.php
@@ -1,4 +1,6 @@
-<?php defined('ABSPATH') or die('No script please!');
+<?php
+
+defined('ABSPATH') or die('No script please!');
 /*
   Plugin Name: Mini Ajax Cart for WooCommerce
   Plugin URI:  https://hashthemes.com/
@@ -11,77 +13,72 @@
   Domain Path: /languages
   Text Domain: mini-ajax-cart
  */
-include( plugin_dir_path(__FILE__) . '/classes/library-class.php'); 
+include( plugin_dir_path(__FILE__) . '/classes/library-class.php');
 
-if(!class_exists('MAJC_Class')) {
+if (!class_exists('MAJC_Class')) {
 
-  class MAJC_Class extends MAJC_Library {
+    class MAJC_Class extends MAJC_Library {
 
-    function __construct() 
-    {
-      $this->define_constants();
-      
-      add_action( 'plugins_loaded',array( $this, 'majc_text_domain' ));
+        function __construct() {
+            add_action('plugins_loaded', array($this, 'majc_text_domain'));
 
-      $this->includes();
+            $this->define_constants();
+
+            $this->includes();
+        }
+
+        public function define_constants() {
+            defined('MAJC_VERSION') or define('MAJC_VERSION', '1.0.4'); //plugin version
+
+            defined('MAJC_TD') or define('MAJC_TD', 'mini-ajax-cart'); //plugin's text domain
+
+            defined('MAJC_CSS_PREFIX') or define('MAJC_CSS_PREFIX', 'majc-'); //plugin's text domain
+
+            defined('MAJC_IMG_DIR') or define('MAJC_IMG_DIR', plugin_dir_url(__FILE__) . 'assets/img/');
+
+            defined('MAJC_BACKEND_IMG_DIR') or define('MAJC_BACKEND_IMG_DIR', plugin_dir_url(__FILE__) . 'assets/backend/img/');
+
+            defined('MAJC_FRONTEND_IMG_DIR') or define('MAJC_FRONTEND_IMG_DIR', plugin_dir_url(__FILE__) . 'assets/frontend/img/');
+
+            defined('MAJC_BACKEND_JS_DIR') or define('MAJC_BACKEND_JS_DIR', plugin_dir_url(__FILE__) . 'assets/backend/js/');
+
+            defined('MAJC_FRONTEND_JS_DIR') or define('MAJC_FRONTEND_JS_DIR', plugin_dir_url(__FILE__) . 'assets/frontend/js/');
+
+            defined('MAJC_BACKEND_CSS_DIR') or define('MAJC_BACKEND_CSS_DIR', plugin_dir_url(__FILE__) . 'assets/backend/css/');
+
+            defined('MAJC_FRONTEND_CSS_DIR') or define('MAJC_FRONTEND_CSS_DIR', plugin_dir_url(__FILE__) . 'assets/frontend/css/');
+
+            defined('MAJC_PATH') or define('MAJC_PATH', plugin_dir_path(__FILE__));
+
+            defined('MAJC_URL') or define('MAJC_URL', plugin_dir_url(__FILE__));
+        }
+
+        public function includes() {
+            include MAJC_PATH . '/Mobile_Detect.php';
+
+            include plugin_dir_path(__FILE__) . '/font-icons.php';
+
+            include plugin_dir_path(__FILE__) . '/google-fonts-list.php';
+
+            include MAJC_PATH . '/classes/enqueue-class.php';
+
+            include MAJC_PATH . '/classes/backend-class.php';
+
+            include MAJC_PATH . '/classes/frontend-class.php';
+        }
+
+        public function majc_text_domain() {
+            load_plugin_textdomain('mini-ajax-cart', false, plugin_dir_url(__FILE__) . 'languages');
+        }
+
     }
 
-    
+    $woo_plugin_path = trailingslashit(WP_PLUGIN_DIR) . 'woocommerce/woocommerce.php';
+    $uwc_plugin_path = trailingslashit(WP_PLUGIN_DIR) . 'ultimate-woocommerce-cart/ultimate-woocommerce-cart.php';
 
-    public function define_constants()
-    {
-      defined('MAJC_VERSION') or define('MAJC_VERSION', '1.0.4'); //plugin version
-
-      defined('MAJC_TD') or define('MAJC_TD', 'mini-ajax-cart'); //plugin's text domain
-
-      defined('MAJC_CSS_PREFIX') or define('MAJC_CSS_PREFIX', 'majc-'); //plugin's text domain
-
-      defined('MAJC_IMG_DIR') or define('MAJC_IMG_DIR', plugin_dir_url(__FILE__) .'assets/img/');
-
-      defined('MAJC_BACKEND_IMG_DIR') or define('MAJC_BACKEND_IMG_DIR', plugin_dir_url(__FILE__) .'assets/backend/img/'); 
-
-      defined('MAJC_FRONTEND_IMG_DIR') or define('MAJC_FRONTEND_IMG_DIR', plugin_dir_url(__FILE__) . 'assets/frontend/img/'); 
-
-      defined('MAJC_BACKEND_JS_DIR') or define('MAJC_BACKEND_JS_DIR', plugin_dir_url(__FILE__) . 'assets/backend/js/');  
-
-      defined('MAJC_FRONTEND_JS_DIR') or define('MAJC_FRONTEND_JS_DIR', plugin_dir_url(__FILE__) . 'assets/frontend/js/');  
-
-      defined('MAJC_BACKEND_CSS_DIR') or define('MAJC_BACKEND_CSS_DIR', plugin_dir_url(__FILE__) . 'assets/backend/css/'); 
-
-      defined('MAJC_FRONTEND_CSS_DIR') or define('MAJC_FRONTEND_CSS_DIR', plugin_dir_url(__FILE__) . 'assets/frontend/css/'); 
-
-      defined('MAJC_PATH') or define('MAJC_PATH', plugin_dir_path(__FILE__));
-
-      defined('MAJC_URL') or define('MAJC_URL', plugin_dir_url(__FILE__)); 
+    if (in_array($woo_plugin_path, wp_get_active_and_valid_plugins()) || in_array($woo_plugin_path, wp_get_active_network_plugins())) {
+        if (!in_array($uwc_plugin_path, wp_get_active_and_valid_plugins()) || !in_array($uwc_plugin_path, wp_get_active_network_plugins())) {
+            new MAJC_Class();
+        }
     }
-
-    public function includes()
-    {
-      include MAJC_PATH . '/Mobile_Detect.php';
-
-      include plugin_dir_path(__FILE__) . '/font-icons.php'; 
-
-      include plugin_dir_path(__FILE__) . '/google-fonts-list.php';
-      
-      include MAJC_PATH . '/classes/enqueue-class.php';  
-
-      include MAJC_PATH . '/classes/backend-class.php';
-
-      if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-
-        include MAJC_PATH . '/classes/frontend-class.php';
-      }
-    }
-
-
-    public function majc_text_domain()
-    {
-      load_plugin_textdomain( 'mini-ajax-cart', false, plugin_dir_url( __FILE__ ).'languages' ); 
-    } 
-  }
-
-  $active_plugins = get_option('active_plugins') ? get_option('active_plugins') : array();
-  if(!in_array('ultimate-woocommerce-cart/ultimate-woocommerce-cart.php', $active_plugins)) {
-    new MAJC_Class();
-  }
 }
