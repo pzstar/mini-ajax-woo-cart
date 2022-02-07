0c212554497633114b8669a30d3eb11182aba4f0
Prabin Dangol <neprabin@gmail.com>
2022-02-07 23:07:30 +0545

    v 1.1.1

--- assets/backend/js/admin-script.js
+++ assets/backend/js/admin-script.js
@@ -25,53 +25,49 @@
         });
 
         // Call all the necessary functions for Icon Picker
-        iconPickerFunction();
-        function iconPickerFunction() {
-            // FontAwesome Icon Control JS
-            $('body').on('click', '.majc-icon-box-wrap .majc-icon-list li', function () {
-                var icon_class = $(this).find('i').attr('class');
-                $(this).closest('.majc-icon-box').find('.majc-icon-list li').removeClass('icon-active');
-                $(this).addClass('icon-active');
-                $(this).closest('.majc-icon-box').prev('.majc-selected-icon').children('i').attr('class', '').addClass(icon_class);
-                $(this).closest('.majc-icon-box').next('input').val(icon_class).trigger('change');
-                $(this).closest('.majc-icon-box').slideUp();
-            });
+        $('body').on('click', '.majc-icon-box-wrap .majc-icon-list li', function () {
+            var icon_class = $(this).find('i').attr('class');
+            $(this).closest('.majc-icon-box').find('.majc-icon-list li').removeClass('icon-active');
+            $(this).addClass('icon-active');
+            $(this).closest('.majc-icon-box').prev('.majc-selected-icon').children('i').attr('class', '').addClass(icon_class);
+            $(this).closest('.majc-icon-box').next('input').val(icon_class).trigger('change');
+            $(this).closest('.majc-icon-box').slideUp();
+        });
 
-            $('body').on('click', '.majc-icon-box-wrap .majc-selected-icon', function () {
-                $(this).next().slideToggle();
-            });
+        $('body').on('click', '.majc-icon-box-wrap .majc-selected-icon', function () {
+            $(this).next().slideToggle();
+        });
 
-            $('body').on('change', '.majc-icon-box-wrap .majc-icon-search select', function () {
-                var selected = $(this).val();
-                $(this).parents('.majc-icon-box').find('.majc-icon-search-input').val('');
-                $(this).parents('.majc-icon-box').children('.majc-icon-list').hide().removeClass('active');
-                $(this).parents('.majc-icon-box').children('.' + selected).fadeIn().addClass('active');
-                $(this).parents('.majc-icon-box').children('.' + selected).find('li').show();
-            });
+        $('body').on('change', '.majc-icon-box-wrap .majc-icon-search select', function () {
+            var selected = $(this).val();
+            $(this).parents('.majc-icon-box').find('.majc-icon-search-input').val('');
+            $(this).parents('.majc-icon-box').children('.majc-icon-list').hide().removeClass('active');
+            $(this).parents('.majc-icon-box').children('.' + selected).fadeIn().addClass('active');
+            $(this).parents('.majc-icon-box').children('.' + selected).find('li').show();
+        });
 
-            $('body').on('keyup', '.majc-icon-box-wrap .majc-icon-search input', function (e) {
-                var $input = $(this);
-                var keyword = $input.val().toLowerCase();
-                var search_criteria = $input.closest('.majc-icon-box').find('.majc-icon-list.active i');
-                delay(function () {
-                    $(search_criteria).each(function () {
-                        if ($(this).attr('class').indexOf(keyword) > -1) {
-                            $(this).parent().show();
-                        } else {
-                            $(this).parent().hide();
-                        }
-                    });
-                }, 500);
-            });
+        $('body').on('keyup', '.majc-icon-box-wrap .majc-icon-search input', function (e) {
+            var $input = $(this);
+            var keyword = $input.val().toLowerCase();
+            var search_criteria = $input.closest('.majc-icon-box').find('.majc-icon-list.active i');
+            delay(function () {
+                $(search_criteria).each(function () {
+                    if ($(this).attr('class').indexOf(keyword) > -1) {
+                        $(this).parent().show();
+                    } else {
+                        $(this).parent().hide();
+                    }
+                });
+            }, 500);
+        });
 
-            var delay = (function () {
-                var timer = 0;
-                return function (callback, ms) {
-                    clearTimeout(timer);
-                    timer = setTimeout(callback, ms);
-                };
-            })();
-        }
+        var delay = (function () {
+            var timer = 0;
+            return function (callback, ms) {
+                clearTimeout(timer);
+                timer = setTimeout(callback, ms);
+            };
+        })();
 
         /* Custom Image Upload */
         $('body').on('click', '.majc-image-upload', function (e) {
@@ -98,7 +94,6 @@
         /* Remove Uploaded Custom Image */
         $('body').on('click', '.majc-image-remove', function () {
             $(this).closest('.majc-icon-image-uploader').find('.majc-custom-menu-image-icon').html('');
-
             $(this).closest('.majc-icon-image-uploader').find('.majc-upload-background-url').val('');
         });
 
