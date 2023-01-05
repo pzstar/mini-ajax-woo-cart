61a73d30f454381657cd943d0b1e1c220cfda733
kishan007 <hashthemesu4@gmail.com>
2023-01-05 16:49:34 +0545

    updated

--- inc/backend/metabox/boxes/main-settings.php
+++ inc/backend/metabox/boxes/main-settings.php
@@ -108,18 +108,22 @@ $display = isset($majc_settings['display']) ? $majc_settings['display'] : null;
                             <label for="majc_search_pages"><?php esc_html_e('Search Page', 'mini-ajax-cart'); ?></label>
                         </p>
 
-                        <p>
-                            <input type="checkbox" name="majc_settings[display][single_pages]" id="majc_single_pages" <?php
-                            if (isset($display['single_pages'])) {
-                                checked($display['single_pages'], 'on', true);
+                        <?php
+                        $post_types = get_post_types(array('public' => 'false'));
+                        sort($post_types);
+                        foreach ($post_types as $post_type) {
+                            if (!($post_type == 'attachment') and get_posts(['post_type' => $post_type])) {
+                                ?>
+                                <p>
+                                    <input type="checkbox" name="majc_settings[display][cpt_pages][]" class="majc-hide-show-cpt-posts" id="majc-hscpt-<?php echo esc_attr($post_type); ?>" data-posttype="<?php echo esc_attr($post_type); ?>" value="<?php echo esc_attr($post_type); ?>" <?php echo in_array(esc_attr($post_type), $display['cpt_pages']) ? 'checked="checked"' : '' ?>/>
+                                    <label for="majc-hscpt-<?php echo esc_attr($post_type); ?>"><?php echo esc_html('All ' . ucwords($post_type)); ?></label>
+                                </p>
+                                <?php
                             }
-                            ?>/>
-                            <label for="majc_single_pages"><?php esc_html_e('All Single Page', 'mini-ajax-cart'); ?></label>
-                        </p>
-
+                        }
+                        ?>
                     </div>
                 </div>
-
 
                 <?php
                 $majc_specific_page = array();
@@ -130,13 +134,38 @@ $display = isset($majc_settings['display']) ? $majc_settings['display'] : null;
                 }
                 $post_types = get_post_types(array('public' => 'false'));
                 sort($post_types);
+                ?>
+                <div class="majc-hide-singular" id="majc-show-archive" <?php echo $display['archive_pages'] == 'on' ? 'style="display:none;"' : '' ?>>
+                    <h4>
+                        <?php
+                        esc_html_e('Specific Archive Page', 'ultimate-woocommerce-cart');
+                        ?>
+                        <!-- <span class="toggle-indicator" aria-hidden="true"></span> -->
+                    </h4>
+
+                    <div class="majc-toggle-tab-body">
+                        <?php
+                        foreach ($post_types as $post_type) {
+                            if (!($post_type == 'attachment' || $post_type == 'majc-cart-template')) {
+                                ?>
+                                <p>
+                                    <input type="checkbox" name="majc_settings[display][specific_archive][]" id="majc-archive-<?php echo esc_attr($post_type); ?>" value="<?php echo esc_attr($post_type); ?>" <?php if (in_array($post_type, $display['specific_archive'])) echo 'checked'; ?>  />
+                                    <label for="majc-archive-<?php echo esc_attr($post_type); ?>"><?php echo esc_html(ucwords($post_type)); ?></label>
+                                </p>
+                                <?php
+                            }
+                        }
+                        ?>
+                    </div>
+                </div>
+                <?php
                 foreach ($post_types as $post_type) {
                     if (!($post_type == 'attachment')) {
                         $posts_loop = new WP_Query(array('post_type' => $post_type, 'posts_per_page' => -1, 'post_status' => 'publish'));
 
                         if (!empty($posts_loop->posts)) {
                             ?>
-                            <div class="majc-hide-singular" <?php if (isset($display['single_pages'])) echo 'style="display:none;"'; ?>>
+                            <div class="majc-hide-singular" id="majc-cpt-<?php echo esc_attr($post_type); ?>" <?php echo in_array(esc_attr($post_type), $display['cpt_pages']) ? 'style="display:none;"' : '' ?>>
 
                                 <h4>
                                     <?php
