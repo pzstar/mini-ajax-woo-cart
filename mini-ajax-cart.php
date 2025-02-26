f59f773116179fcaa7fae6c6462baaa1e7da46c8
Prabin Dangol <neprabin@gmail.com>
2025-02-26 16:29:08 +0545

    Update mini-ajax-cart.php

--- mini-ajax-cart.php
+++ mini-ajax-cart.php
@@ -12,7 +12,6 @@ defined('ABSPATH') or die('No script please!');
   License URI: https://www.gnu.org/licenses/gpl-2.0.html
   Domain Path: /languages
   Text Domain: mini-ajax-cart
-  Requires Plugins: woocommerce
  */
 
 defined('MAJC_FILE') or define('MAJC_FILE', __FILE__);
